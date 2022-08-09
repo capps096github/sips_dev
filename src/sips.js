3e64fa04c41118a3da593eed9ef14e27687d6246
C APPS GITHUB TEAM <84335028+capps096github@users.noreply.github.com>
2022-08-09 10:36:44 +0300

    Updated Login

--- src/sips.js
+++ src/sips.js
@@ -32,3 +32,37 @@ function goToRegister(from) {
       break;
   }
 }
+
+
+// * Go to Customers Register
+function goToCustomersLogin() {
+  location.href = "customers_login.html";
+}
+
+// * Go to Admin Register
+function goToAdminLogin() {
+  location.href = "admin_login.html";
+}
+
+// * Go to Customers Register
+function goToCustomersRegister() {
+  location.href = "customer_register.html";
+}
+
+// * Go to Admin Register
+function goToAdminRegister() {
+  location.href = "admin_register.html";
+}
+
+// ? GO to Splash Screen
+function goToSplash(from) {
+  switch (from) {
+    case "index":
+      location.href = "splash.html";
+      break;
+
+    default:
+      location.href = "../splash.html";
+      break;
+  }
+}
